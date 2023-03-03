71aa71b9de0a66f73bbf6da9e28aac28afe92a55
JayOReilly <99806112+JayOReilly@users.noreply.github.com>
2023-03-03 19:11:46 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Parallax, ParallaxLayer} from '@react-spring/parallax';
+import { useRef } from 'react';
+import earth from '../src/images/night.jpg';
+import space from '../src/images/solar.jpg';
+import sea from '../src/images/water.jpg';
+import world from '../src/images/world.jpg';
+
 
 function App() {
+
+  const ref = useRef()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+    <Parallax pages={1} ref={ref}>
+      
+      
+      <ParallaxLayer  
+      speed={1}
+      style={{
+        backgroundImage: `url(${world})`,
+        backgroundSize: 'cover',
+      }}
+      >
+        <h2>Welcome</h2>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={1} 
+      speed={1.5}
+      style={{
+        backgroundImage: `url(${earth})`,
+        backgroundSize: 'cover',
+        
+      }}
+      
+      >
+        
+      </ParallaxLayer>
+
+      
+
+     
+
+    </Parallax>
+
     </div>
   );
 }
